3d9da307ce874d1fa326bac35e37b6464a78eb6a
Paul Dinghin <paul.dinghin@etu.u-bordeaux.fr>
2025-11-26 18:06:43 +0100

    start of preprocessing function

---
+++ data_processing/preprocessing.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pandas as pd
+import numpy as np
+import sys
+
+#pip install pandas 
+file =  sys.argv[1]
+usecol = ['id','name','host_id','host_name','host_response_rate','host_is_superhost',\
+              'latitude','longitude','property_type','room_type','bathrooms','bedrooms',\
+                'price','minimum_nights','maximum_nights','availability_365','number_of_reviews',\
+                    'review_scores_rating','review_scores_cleanliness','license']
+data = pd.read_csv(file,sep=",",usecols= usecol)
+#1 if host have license else 0
+data['license'] = np.where(data['license'] == 'Exempt',0,1)
+print(data.head())
