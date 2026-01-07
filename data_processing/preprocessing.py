aaf0e1e3348a61e789c123342082299bc25fa6fa
Paul Dinghin <paul.dinghin@etu.u-bordeaux.fr>
2026-01-07 15:32:03 +0100

    preprocessing close #10

--- data_processing/preprocessing.py
+++ data_processing/preprocessing.py
@@ -3,13 +3,44 @@ import pandas as pd
 import numpy as np
 import sys
 
-#pip install pandas 
-file =  sys.argv[1]
-usecol = ['id','name','host_id','host_name','host_response_rate','host_is_superhost',\
-              'latitude','longitude','property_type','room_type','bathrooms','bedrooms',\
-                'price','minimum_nights','maximum_nights','availability_365','number_of_reviews',\
-                    'review_scores_rating','review_scores_cleanliness','license']
-data = pd.read_csv(file,sep=",",usecols= usecol)
-filter = data[data["room_type"]!="Private room"].copy()
-filter['license'] = filter['license'].notna().astype(int)
-filter.to_csv('data/pre_'+file,index=False)
+import json
+from shapely.geometry import shape, Point
+
+
+csv_file =  sys.argv[1]
+
+geojson_file = "../epci-243300316-sections.json"  
+
+usecol = [
+    'id','name','host_id','host_name','host_response_rate','host_is_superhost',
+    'latitude','longitude','property_type','room_type','bathrooms','bedrooms',
+    'price','minimum_nights','maximum_nights','number_of_reviews',
+    'review_scores_rating','review_scores_cleanliness','license'
+]
+
+
+data = pd.read_csv(csv_file, sep=",", usecols=usecol)
+
+with open(geojson_file, "r", encoding="utf-8") as f:
+    geojson = json.load(f)
+
+
+polygons = []
+for feature in geojson["features"]:
+    geom = shape(feature["geometry"])     
+    prop_id = feature["properties"]["id"]
+    polygons.append((geom, prop_id))
+
+def find_properties_id(row):
+    point = Point(row["longitude"], row["latitude"])
+
+    for polygon, prop_id in polygons:
+        if polygon.contains(point):
+            return prop_id
+
+    return None 
+
+
+data["properties_id"] = data.apply(find_properties_id, axis=1)
+
+data.to_csv("../data/data_with_properties_id.csv", index=False)
---
+++ env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ENV_NAME="fouille_env"
+PYTHON_VERSION="3"
+python$PYTHON_VERSION -m venv $ENV_NAME
+source $ENV_NAME/bin/activate
+pip install --upgrade pip
+pip install numpy pandas geopandas
