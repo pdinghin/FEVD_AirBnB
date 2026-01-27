4aa73a9ed53d8088800b49c4b379682ffc1fa170
Paul Dinghin <paul.dinghin@etu.u-bordeaux.fr>
2026-01-27 11:58:32 +0100

    fin close #20

--- env.sh
+++ env.sh
@@ -4,6 +4,6 @@ ENV_NAME="fouille_env"
 PYTHON_VERSION="3"
 python$PYTHON_VERSION -m venv $ENV_NAME
 source $ENV_NAME/bin/activate
-pip install --upgrade pip
+
 pip install numpy pandas geopandas matplotlib
-pip install scikit-learn
+pip install scikit-learn seaborn
